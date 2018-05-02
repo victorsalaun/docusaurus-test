0d3130443347658bf0ef76371b38a8866d7c8584
victorsalaun <victor.salaun@gmail.com>
2018-05-02 11:28:56 +0200

    update project name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,14 +21,14 @@ const users = [
 const siteConfig = {
   title: 'Test Site' /* title for your website */,
   tagline: 'A website for testing',
-  url: 'https://victorsalaun.github.io/docusaurus-test/' /* your website url */,
-  baseUrl: 'https://victorsalaun.github.io/docusaurus-test/' /* base url for your project */,
+  url: 'https://victorsalaun.github.io/hesperides-documentation' /* your website url */,
+  baseUrl: 'https://victorsalaun.github.io/hesperides-documentation/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docusaurus-test',
+  projectName: 'hesperides-documentation',
   organizationName: 'victorsalaun',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
